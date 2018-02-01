96489f7cc2dd72468056dc155869fca319e2c5e2
vineet <vineet.12cs069@abes.ac.in>
2018-02-01 09:32:56 +0530

    working component

--- src/app/component/Home.js
+++ src/app/component/Home.js
@@ -1,8 +1,26 @@
 import React from "react"; 
 import { render } from "react-dom";
+import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
-
+    constructor(props){
+        super();
+        this.state = {
+            user: props.user
+        };
+    }
+    onClickOlder(){
+        console.log(this.state.user);
+        this.setState({
+            user: {
+                name: this.state.user.name,
+                city: this.state.user.city,
+                age: this.state.user.age + 3,
+                hobbies: this.state.user.hobbies
+            }
+        })
+        console.log(this.state.user);
+    }
     render(){
         return(
             <div className="container" style={{marginTop: 50 + 'px'}}>
@@ -21,7 +39,39 @@ export class Home extends React.Component {
                         <p>A fixed navigation bar stays visible in a fixed position (top or bottom) independent of the page scroll.</p> 
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col-md-6 text-center">
+                        <h2>User Info</h2>
+                        <div>
+                        <label>Name:</label>
+                        <span>{this.state.user.name}</span>
+                        </div>
+                        <br></br>
+                        <div>
+                        <label>Age:</label>
+                        <span>{this.state.user.age}</span>
+                        <button onClick={() => this.onClickOlder()} className="btn btn-primary">make me older</button>
+                        </div>
+                        <br></br>
+                        <div>
+                        <label>City:</label>
+                        <span>{this.state.user.city}</span>
+                        </div>
+                        <br></br>
+                        <div>
+                        <label>Hobies:</label>
+                        <ol>{this.state.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}</ol>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
+
+Home.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    city: PropTypes.string,
+    user: PropTypes.object
+};
--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,23 @@
-import React from "react"; 
+import React from "react";
 import { render } from "react-dom";
 import { Home } from "./component/Home";
 import { Header } from "./component/Header";
 
-class App extends React.Component{
+class App extends React.Component {
     render() {
-        return (
-            <div className="" >
-                <Header/>
-                <Home/>
+        var user = {
+            name: "vineet",
+            age: 24,
+            city: "Delhi",
+            hobbies: ["Playing Cricket", "Listening Songs", "Reading"]
+        };
+        var isUserShow = true;
+        return ( <div>
+            <Header/>
+            <Home user={user} isUserShow={isUserShow}/>
             </div>
         );
     }
 }
 
-render (<App/>, window.document.getElementById("app"));
+render( < App/> , window.document.getElementById("app"));
